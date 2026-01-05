ae8975cdb082bcab4af762ed482cfacfc0406949
Abdelrahman <Aa5282243@gmail.com>
2026-01-05 19:29:32 +0200

    feat: Configure and initialize Firebase across Android and iOS platforms.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sehhago/firebase_options.dart';
 import 'package:sehhago/sehha_go_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SehhaGo());
 }
